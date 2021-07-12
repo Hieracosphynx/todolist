8269bfd77248d3e8904374fc8212a5a7ccc6716b
Hieracosphynx <cortezmiguel05@gmail.com>
2021-07-12 10:39:57 -0700

    Experimenting with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ToDoTasks from './components/ToDoTasks/ToDoTasks';
 import NewToDo from './components/NewToDo/NewToDo';
 import LogIn from './components/LogIn/LogIn';
@@ -8,18 +8,38 @@ import AuthContext from './context/auth-context';
 import './App.css';
 
 const App = () => {
+  const [showElement, setShowElement] = useState();
   const [toDoTasks, setToDoTasks] = useState([]);
 
   const authCtx = useContext(AuthContext);
+
+  const { isLoggedIn: loggedIn } = authCtx;
 
   const addTaskHandler = (task) => {
     setToDoTasks((prevTasks) => {
       return [...prevTasks, task];
     });
   };
+
+  const showElementHandler = () => {
+    setShowElement(true);
+  };
+
+  useEffect(() => {
+    if (showElement) {
+      const timer = setTimeout(() => {
+        setShowElement(false);
+      }, 300);
+    }
+
+    return () => {};
+  }, [showElement]);
+
   return (
     <>
       <NavBar />
+      <button onClick={showElementHandler}>Click Me!</button>
+      {showElement && <p>dfdfdf</p>}
       {authCtx.isLoggedIn && (
         <>
           <NewToDo addTask={addTaskHandler} />
